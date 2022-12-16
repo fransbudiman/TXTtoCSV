2d5e51071fab4ec371ecc2e3646ef1c8dab0e9ad
Frans Budiman <fransrewl@gmail.com>
2022-12-16 06:48:21 -0500

    updated version

--- txt_to_CSV_Version1.py
+++ txt_to_CSV_Version1.py
@@ -1,23 +1,27 @@
+import glob
+import pandas as pd
 import csv
 
 
-print("START")
+for file in glob.glob("\\UNIFRANS\\Others\\testing\\*.xlsx"):
+    print(file)
+    read_file = pd.read_excel(file)
+    backName = file.split("\\")[-1]
+    backName = backName.split(".")[0]
+    print(backName)
+    read_file.to_csv("\\UNIFRANS\\Others\\testing\\"+backName + "_split.csv", index = None)
+    outputHandle = open("\\UNIFRANS\\Others\\testing\\"+backName + "_final.csv", 'w', newline = None)
+    writer = csv.writer(outputHandle)
+    
+    with open("\\UNIFRANS\\Others\\testing\\"+backName + "_split.csv", 'r') as f:
+        for row in f:
+            row = row.replace("\n","")
+            listRow = row.split(" ")
+            print(listRow)
+            writer.writerow(listRow)
+    outputHandle.close()
+    
 
-openHandle = open("C:\\UNIFRANS\\Others\\testing\\AAATesting.txt", 'r')
-outputHandle  =  open("\\UNIFRANS\\Others\\testing2\\" + "OutputSample" + ".csv" ,'w', newline = "")
-
-writer = csv.writer(outputHandle)
-
-content = openHandle.read()
-
-rowSplitList = content.split('\n') #splits big String into list of rows
-
-for rowItem in rowSplitList:
-    cellSplitList = rowItem.split() #splits rows separated by spaces into list of individual cell content
-    writer.writerow(cellSplitList)
-
-openHandle.close()
-outputHandle.close()
-
-
-print("DONE")
+            
+    
+print("DONE!")
